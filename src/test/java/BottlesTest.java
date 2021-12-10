4ffe7b470e970006c37b1560b91853b09d7a4dc8
Cosimo Scarcella <cosimo.scarcella@ynap.com>
2021-12-10 17:55:55 +0100

    initial commit

---
+++ src/test/java/BottlesTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class BottlesTest {
+    @Test
+    void name() {
+        assertEquals(true, true);
+    }
+}
